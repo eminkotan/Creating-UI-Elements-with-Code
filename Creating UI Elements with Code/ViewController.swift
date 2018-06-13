22dd7cf5d7c76b07abf19144f50a42e1566846a7
Emin Kotan <emin@emin-macbook-air.local>
2018-06-13 21:37:25 +0300

    Initial Commit

---
+++ Creating UI Elements with Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Creating UI Elements with Code
+//
+//  Created by Emin Kotan on 13.06.2018.
+//  Copyright © 2018 Emin Kotan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
